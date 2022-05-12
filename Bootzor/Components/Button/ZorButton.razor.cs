70edcbef45d5d70b42e0432af7cf0ccd14cedf0a
hddisseny <hddisseny@gmail.com>
2022-05-13 00:26:12 +0200

    First attempt adding button minimal logic and style

---
+++ Bootzor/Components/Button/ZorButton.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Bootzor.Components.Button;
+
+public partial class ZorButton : ComponentBase
+{
+ 
+    private ElementReference zorButton;
+
+    [Parameter, EditorRequired]
+    public EventCallback ClickButton { get; set; }
+
+    [Parameter, EditorRequired]
+    public RenderFragment? Content { get; set; }
+
+}
